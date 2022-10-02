136f48e3c2c294431afadf5bb628f503631c7441
ilya smagin <ilya-z1999@ya.ru>
2022-10-02 13:37:09 +0300

    second settings

--- pages/user.js
+++ pages/user.js
@@ -46,34 +46,98 @@ export default function IndexPage() {
         </div>
       ) : (
         <>
-          <button
-            className="absolute z-20 top-4 right-4 rounded-lg bg-transparent border border-gray-500 w-14 h-14 flex justify-center items-center"
-            onClick={async () => {
-              const { error } = await supabase.auth.signOut();
-              if (error) console.log("Error logging out:", error.message);
-            }}
-          >
-            <svg
-              className="w-12 h-12 text-gray-500"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+          {!isSettingsOpen ? (
+            <button
+              onClick={() => {
+                setIsSettingsOpen(!isSettingsOpen);
+              }}
+              className="absolute z-20 top-4 right-4 rounded-lg bg-transparent border border-gray-500 w-14 h-14 flex justify-center items-center"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1"
-                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-              ></path>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              ></path>
-            </svg>
-          </button>
+              <svg
+                className="w-12 h-12 text-gray-500"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1"
+                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                ></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                ></path>
+              </svg>
+            </button>
+          ) : (
+            <div className="absolute top-20 w-max-md rounded-2xl w-3/4 h-fit bg-white z-30">
+              <ul className="r">
+                <li className="mx-3 my-2 flex flex-row justify-between items-center flex-nowrap">
+                  <span className="w-48 p-2 ">аватар</span>
+                  <img
+                    className="w-16 h-16 rounded-2xl"
+                    src="https://github.com/rfeskov/r.feskov/blob/main/photo_2022-10-02%2012.34.41.jpeg?raw=true"
+                  />
+                </li>
+                <li className="flex flex-row justify-between flex-nowrap">
+                  <span className="mx-3 my-2 w-48 px-2 ">имя</span>
+                  <span className="mx-3 my-2 w-48 px-2 text-right">
+                    Владимиран
+                  </span>
+                </li>
+
+                <li className="flex flex-row justify-between flex-nowrap">
+                  <span className="mx-3 my-2 w-48 px-2 ">никнейм</span>
+                  <input
+                    className="mx-3 my-2 w-1/2 px-2 placeholder:text-black text-right"
+                    type="text"
+                    placeholder={username || "введите ник"}
+                    value={username}
+                    required
+                    onChange={(e) => {
+                      setError("");
+                      setUsername(e.target.value.toLowerCase());
+                    }}
+                  />
+                </li>
+                <li className="flex flex-row justify-between flex-nowrap">
+                  <span className="mx-3 my-2 w-48 px-2 ">дата рождения</span>
+                  <span className="mx-3 my-2 w-48 px-2 text-right">
+                    11.05.2000
+                  </span>
+                </li>
+                <li className="flex flex-row justify-between flex-nowrap">
+                  <span className="mx-3 my-2 w-48 px-2 ">друзей</span>
+                  <span className="mx-3 my-2 w-48 px-2 text-right">5</span>
+                </li>
+                <li className="cursor-pointer flex flex-row justify-between flex-nowrap">
+                  <span className="mx-3 my-2 w-48 px-2 ">
+                    пригласить друзей
+                  </span>
+                </li>
+                <li className="cursor-pointer flex flex-row justify-between flex-nowrap">
+                  <span className="mx-3 my-2 w-48 px-2 ">черный список</span>
+                </li>
+                <li className="cursor-pointer flex flex-row justify-between flex-nowrap">
+                  <span className="mx-3 my-2 w-48 px-2 ">уведомления</span>
+                </li>
+                <li
+                  className="cursor-pointer mx-3 my-2 w-48 px-2 text-gray-600"
+                  onClick={async () => {
+                    const { error } = await supabase.auth.signOut();
+                    if (error) console.log("Error logging out:", error.message);
+                  }}
+                >
+                  Выйти из аккаунта
+                </li>
+              </ul>
+            </div>
+          )}
           <div className="max-w-lg relative z-10 w-full h-full flex flex-col justify-center items-center p-4">
             <TodoList user={supabase.auth.user()} />
           </div>
